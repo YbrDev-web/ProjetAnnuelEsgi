476fc3f741f927df8e211fb6652ae1c91b653dc4
YbrDev-web <ybenromdhane2003@gmail.com>
2025-05-26 13:28:51 +0200

    routes fonctionnels

--- Kanboard-app/resources/views/layouts/navigation.blade.php
+++ Kanboard-app/resources/views/layouts/navigation.blade.php
@@ -24,8 +24,8 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link>
-                        <strong>{{ __('Ressources') }}</strong>
+                <x-nav-link :href="route('ressources')" :active="request()->routeIs('ressources')">
+                        {{ __('ressources') }}
                     </x-nav-link>
                 </div>
             </div>
