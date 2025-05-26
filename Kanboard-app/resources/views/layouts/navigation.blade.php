1901d4ea6c961942c47c00b0d44a4d33abb823ce
YbrDev-web <ybenromdhane2003@gmail.com>
2025-05-26 13:38:00 +0200

    route vers la page des offres

--- Kanboard-app/resources/views/layouts/navigation.blade.php
+++ Kanboard-app/resources/views/layouts/navigation.blade.php
@@ -14,8 +14,8 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link>
-                        <b>{{ __('offres') }}</b>
+                <x-nav-link :href="route('Offres')" :active="request()->routeIs('Offres')">
+                        {{ __('Offres') }}
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
